8e9bce069db128a09482f9649a6e8c21114eebac
Joey <thejosephacevedo@gmail.com>
2024-01-23 20:23:07 -0500

    Basic app is live with index.html setup

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask('__name__')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
 
 @app.route('/')
 
